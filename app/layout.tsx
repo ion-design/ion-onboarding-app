c3ba998a5f08547093271ca08b4487cf6a6bdabe
Samraaj Bath <5092624+Samraaj@users.noreply.github.com>
2024-03-06 00:03:38 -0800

    center default for look

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,29 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-   title: "ion design",
-   description: "Ship product at the speed of thought",
+	title: 'ion design',
+	description: 'Ship product at the speed of thought',
 };
 
 export default function RootLayout({
-   children,
+	children,
 }: Readonly<{
-   children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-   return (
-      <html lang="en">
-         <body className={inter.className + "w-screen h-screen bg-base overflow-y-scroll"}>{children}</body>
-      </html>
-   );
+	return (
+		<html lang="en">
+			<body
+				className={
+					inter.className +
+					'w-screen h-screen bg-base overflow-y-scroll flex items-center justify-center'
+				}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
